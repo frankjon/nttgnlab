eaa651b1f16f95f2e2572c919c940511c3214fb8
frankjon <48197028+frankjon@users.noreply.github.com>
2023-09-05 13:57:55 +0530

    Create scatterplot.py

---
+++ scatterplot.py
@@ -0,0 +1,24 @@
+# import all important libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# load dataset
+data = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
+
+# convert to dataframe
+df = pd.read_csv(data)
+
+# display top most rows
+df.head()
+
+# depict scatterplot illustration
+sns.set_context("talk", font_scale=1.1)
+plt.figure(figsize=(8, 6))
+sns.scatterplot(x="sepal.length",
+				y="sepal.width",
+				data=df)
+
+# assign labels
+plt.xlabel("Sepal.Length")
+plt.ylabel("sepal.width")
