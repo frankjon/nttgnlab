638282f7f5284553bf44cc579c20b2ec61a7b0cd
frankjon <48197028+frankjon@users.noreply.github.com>
2023-10-23 10:14:34 +0530

    Create ncharinstrusingloop.py

---
+++ ncharinstrusingloop.py
@@ -0,0 +1,15 @@
+# get input
+Str = "Geeks For Geeks!"
+N = 4
+
+# print the string
+print(Str)
+
+# iterate loop
+while(N > 0):
+
+# print character
+	print(Str[-N], end='')
+
+	# decrement the value of N
+	N = N-1
