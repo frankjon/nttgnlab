9cf9d6fcb7217a84b1b7cd4044b18b61ed8f84bf
frankjon <48197028+frankjon@users.noreply.github.com>
2023-11-06 09:44:53 +0530

    Create strslicingpositiveindexing.py

---
+++ strslicingpositiveindexing.py
@@ -0,0 +1,15 @@
+# get input
+Str = "Geeks For Geeks!"
+N = 4
+
+# print the string
+print(Str)
+
+# get length of string
+length = len(Str)
+
+# create a new string of last N characters
+Str2 = Str[length - N:]
+
+# print Last N characters
+print(Str2)
