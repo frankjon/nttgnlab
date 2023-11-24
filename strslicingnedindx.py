a5f795572f9e2202771f448b3a74fb89312a9d9b
frankjon <48197028+frankjon@users.noreply.github.com>
2023-11-24 10:12:40 +0530

    Create strslicingnedindx.py

---
+++ strslicingnedindx.py
@@ -0,0 +1,12 @@
+# get input
+Str = "Geeks For Geeks!"
+N = 4
+
+# print the string
+print(Str)
+
+# create a new string of last N characters
+Str2 = Str[-N:]
+
+# print Last N characters
+print(Str2)
