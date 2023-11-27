0ecb2bef669a51c2359bbf8771b0d8d945f9ac16
frankjon <48197028+frankjon@users.noreply.github.com>
2023-11-27 10:08:01 +0530

    Create getlastcharinstr.py

---
+++ getlastcharinstr.py
@@ -0,0 +1,9 @@
+Str = "GeeksforGeeks"
+
+#using negative index to pick the last element of the string
+# Here -1 refers to the last character index in the string
+
+last_character = Str[-1]
+
+#printing the last character 
+print(last_character)
