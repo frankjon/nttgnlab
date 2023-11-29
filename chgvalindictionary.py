5db56a2b9bf413a423ec86eb556f1a362ef84f1b
frankjon <48197028+frankjon@users.noreply.github.com>
2023-11-29 11:03:09 +0530

    Create chgvalindictionary.py

---
+++ chgvalindictionary.py
@@ -0,0 +1,11 @@
+# declaring dictionary 
+dict = {'hari': 1, 'dita': 2} 
+
+# original dictionary 
+print("initial dictionary-", dict) 
+
+# changing the key value from 2 to 4 
+dict['dita'] = 4
+
+# dictionary after update 
+print("dictionary after modification-", dict) 
